326552d3a89eb1e88b566e9e6e70938459f11bb2
Andrew Nikonchuk <anikonch@gmail.com>
2018-03-29 15:50:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,6 @@ function addElementToBeginningOfArray(array,element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-   return newArray array.unshift(element);
+  var newArray = array.unshift(element);
+  return newArray;
 }
